1b43987a26a0371e2257aeec687d1c7194bbd024
Shakti Gurung <mr.shakti.gurung@gmail.com>
2019-12-28 11:07:00 +1100

    monster rolodex completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import {CardList} from './components/card-list/CardList';
+import {SearchBox} from './components/search-box/SearchBox';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   };
 
@@ -16,11 +18,21 @@ class App extends Component {
       .then(users=> this.setState({ monsters: users}));
   }
 
+  handleChange = e =>{
+    this.setState({searchField: e.target.value})
+  }
+
   render(){
-    const {monsters} = this.state;
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()) );
     return (
       <div className="App">
-        <CardList monsters={monsters} />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox 
+          placeholder='Search Monsters' 
+          handleChange={this.handleChange} 
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
